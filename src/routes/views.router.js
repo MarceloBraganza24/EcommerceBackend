a4e74f9afc182f54c8597288b44a27f0d46c9f3b
litocode24 <marcelo_braganza@hotmail.com>
2024-03-12 03:46:16 -0300

    proyectoFinalEcommerceCursoBackend_v1.1

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -6,12 +6,12 @@ export default class ViewsRouter extends Router {
     init() {
         this.get('/users-view', [accessRolesEnum.ADMIN], passportStrategiesEnum.JWT, usersView);
         this.get('/carts-view', [accessRolesEnum.ADMIN], passportStrategiesEnum.JWT, cartsView);
-        this.get('/shopping-cart-user-view', [accessRolesEnum.ADMIN, accessRolesEnum.PREMIUM], passportStrategiesEnum.JWT, shoppingCartUserView);
-        this.get('/finalize-purchase-view', [accessRolesEnum.ADMIN, accessRolesEnum.PREMIUM], passportStrategiesEnum.JWT, finalizePurchaseView);
+        this.get('/shopping-cart-user-view', [accessRolesEnum.ADMIN, accessRolesEnum.PREMIUM, accessRolesEnum.USER], passportStrategiesEnum.JWT, shoppingCartUserView);
+        this.get('/finalize-purchase-view', [accessRolesEnum.ADMIN, accessRolesEnum.PREMIUM, accessRolesEnum.USER], passportStrategiesEnum.JWT, finalizePurchaseView);
         this.get('/products-view', [accessRolesEnum.ADMIN, accessRolesEnum.PREMIUM, accessRolesEnum.USER], passportStrategiesEnum.JWT, productsView);
         this.get('/user-register-view', [accessRolesEnum.PUBLIC], passportStrategiesEnum.NOTHING, userRegisterView);
         this.get('/login-view', [accessRolesEnum.PUBLIC], passportStrategiesEnum.NOTHING, loginView);
-        this.get('/resetPass-view', [accessRolesEnum.ADMIN, accessRolesEnum.PREMIUM], passportStrategiesEnum.JWT, resetPassView);
-        this.get('/newPass-view', [accessRolesEnum.ADMIN, accessRolesEnum.PREMIUM], passportStrategiesEnum.JWT, newPassView);
+        this.get('/resetPass-view', [accessRolesEnum.ADMIN, accessRolesEnum.PREMIUM, accessRolesEnum.USER], passportStrategiesEnum.JWT, resetPassView);
+        this.get('/newPass-view', [accessRolesEnum.ADMIN, accessRolesEnum.PREMIUM, accessRolesEnum.USER], passportStrategiesEnum.JWT, newPassView);
     }
 }
